dc802b4267efeb1b52b48259c22942a0e89d6717
大脇健吾 <owaki-kengo@kayac.com>
2019-11-06 14:09:54 +0900

    ts化

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const About = () => (
+    <div>
+        <Head>
+            <title>About</title>
+            <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Nav />
+    </div>
+)
+
+export default About
