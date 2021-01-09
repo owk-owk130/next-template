357e781e622507daebecca4f30facc6d5c2e2e83
owk-owk130 <owaki-kengo@kayac.com>
2021-01-09 23:37:19 +0900

    recoil導入

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.scss";
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 const App = ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />;
+    return (
+        <RecoilRoot>
+            <Component {...pageProps} />;
+        </RecoilRoot>
+    );
 };
 
 export default App;
