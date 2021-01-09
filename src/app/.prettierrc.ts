86fb2f616702d920b876b05dcf89634c2d44d5d4
owk-owk130 <owaki-kengo@kayac.com>
2021-01-09 22:57:25 +0900

    indent

--- src/app/.prettierrc.ts
+++ src/app/.prettierrc.ts
@@ -1,6 +1,6 @@
 module.exports = {
-    "trailingComma": "all",
-    "singleQuote": true,
-    "printWidth": 100,
-    "tabWidth": 2
-}
+    trailingComma: "all",
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 4,
+};
