401e6c172d01b9485b67bb7df89a6f7797373d10
owk-owk130 <owaki-kengo@kayac.com>
2021-02-12 13:47:43 +0900

    recoilに秩序を作る

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,19 +1,18 @@
-import { sampleState } from "../atoms/sample";
-import { useRecoilState } from "recoil";
+import { useSample } from "../atoms/sample";
 
 const Counter = () => {
-    const [sample, setSample] = useRecoilState(sampleState);
-    const handleCountButton = () => {
-        setSample({ counter: sample.counter + 1 });
-    };
-    return (
-        <>
-            <p>{sample.counter}</p>
-            <button type="button" onClick={handleCountButton}>
-                click
-            </button>
-        </>
-    );
+  const [sample, setSample] = useSample();
+  const handleCountButton = () => {
+    setSample({ counter: sample.counter + 1 });
+  };
+  return (
+    <>
+      <p>{sample.counter}</p>
+      <button type="button" onClick={handleCountButton}>
+        click
+      </button>
+    </>
+  );
 };
 
 export default Counter;
